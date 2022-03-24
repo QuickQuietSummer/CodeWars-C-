e4c6ffab3587cfd16b04fe2881e2321fff8f238d
quickquietsummer <ruslan.kokoev.1999@gmail.com>
2022-03-24 20:33:24 +0300

    24.03.2022

---
+++ CodeWars.Test/Tests/24.03.2022/CountingDuplicatesTest.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars.Katas._24._03._2022;
+using NUnit.Framework;
+
+namespace CodeWars.Test.Tests._24._03._2022
+{
+    [TestFixture]
+    public class CountingDuplicatesTest
+    {
+        [Test]
+        public void KataTests()
+        {
+            Assert.AreEqual(0, CountingDuplicates.DuplicateCount(""));
+            Assert.AreEqual(0, CountingDuplicates.DuplicateCount("abcde"));
+            Assert.AreEqual(2, CountingDuplicates.DuplicateCount("aabbcde"));
+            Assert.AreEqual(2, CountingDuplicates.DuplicateCount("aabBcde"), "should ignore case");
+            Assert.AreEqual(1, CountingDuplicates.DuplicateCount("Indivisibility"));
+            Assert.AreEqual(2, CountingDuplicates.DuplicateCount("Indivisibilities"), "characters may not be adjacent");
+            Assert.AreEqual(8, CountingDuplicates.DuplicateCount("165180178190178180184179180178181187165188163188177182183193"));
+        }
+    }
+}
---
+++ CodeWars/Katas/24.03.2022/CountingDuplicates.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars.Katas._24._03._2022
+{
+    public class CountingDuplicates
+    {
+        public static int DuplicateCount(string str)
+        {
+            int result = 0;
+            if (string.IsNullOrEmpty(str)) return result;
+            List<char> charsRaw = str.ToLower().ToCharArray().ToList();
+            charsRaw.RemoveAll(c => c.Equals(' '));
+            List<char> charsUnique = charsRaw.Distinct().ToList();
+            foreach (var eachCharUnique in charsUnique)
+            {
+                var duplicatesCount = charsRaw.RemoveAll(c => c.Equals(eachCharUnique));
+                if (duplicatesCount > 1)
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
