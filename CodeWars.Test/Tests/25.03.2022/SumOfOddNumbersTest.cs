023c17e474a8903edc28ffe7d3129ce482cebdb3
quickquietsummer <ruslan.kokoev.1999@gmail.com>
2022-03-25 11:36:51 +0300

    25.03.2022 SumOfOddNumbers.cs refactor

--- CodeWars.Test/Tests/25.03.2022/SumOfOddNumbersTest.cs
+++ CodeWars.Test/Tests/25.03.2022/SumOfOddNumbersTest.cs
@@ -10,6 +10,7 @@ namespace CodeWars.Test.Tests._25._03._2022
         public void Test1()
         {
             Assert.AreEqual(1, SumOfOddNumbers.RowSumOddNumbers(1));
+            Assert.AreEqual(8, SumOfOddNumbers.RowSumOddNumbers(2));
             Assert.AreEqual(27, SumOfOddNumbers.RowSumOddNumbers(3));
             Assert.AreEqual(74088, SumOfOddNumbers.RowSumOddNumbers(42));
         }
--- CodeWars/Katas/25.03.2022/SumOfOddNumbers.cs
+++ CodeWars/Katas/25.03.2022/SumOfOddNumbers.cs
@@ -6,19 +6,7 @@ namespace CodeWars.Katas._25._03._2022
     {
         public static long RowSumOddNumbers(long n)
         {
-            long firstNumberInRow = 1;
-            for (long row = 1; row <= n; row++)
-            {
-                firstNumberInRow += 2 * (row - 1);
-            }
-
-            long sumInRow = 0;
-            for (long column = 1; column <= n; column++)
-            {
-                sumInRow += firstNumberInRow + 2 * (column - 1);
-            }
-
-            return sumInRow;
+            return n * n * n;
         }
     }
 }
