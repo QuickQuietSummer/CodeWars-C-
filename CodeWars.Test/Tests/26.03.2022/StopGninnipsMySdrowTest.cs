5a15de5b256ecadd6edb00aa0ee421d62258f12d
quickquietsummer <ruslan.kokoev.1999@gmail.com>
2022-03-26 11:19:50 +0300

    26.03.2022

---
+++ CodeWars.Test/Tests/26.03.2022/StopGninnipsMySdrowTest.cs
@@ -0,0 +1,45 @@
+﻿using CodeWars.Katas._26._03._2022;
+using NUnit.Framework;
+
+namespace CodeWars.Test.Tests._26._03._2022
+{
+    [TestFixture]
+    public class StopGninnipsMySdrowTest
+    {
+        [Test]
+        public static void Test1()
+        {
+            Assert.AreEqual("emocleW", StopGninnipsMySdrow.SpinWords("Welcome"));
+        }
+  
+        [Test]
+        public static void Test2()
+        {  
+            Assert.AreEqual("Hey wollef sroirraw", StopGninnipsMySdrow.SpinWords("Hey fellow warriors"));
+        }
+  
+        [Test]
+        public static void Test3()
+        {
+            Assert.AreEqual("This is a test", StopGninnipsMySdrow.SpinWords("This is a test"));
+        }
+  
+        [Test]
+        public static void Test4()
+        {
+            Assert.AreEqual("This is rehtona test", StopGninnipsMySdrow.SpinWords("This is another test"));
+        }
+  
+        [Test]
+        public static void Test5()
+        {
+            Assert.AreEqual("You are tsomla to the last test", StopGninnipsMySdrow.SpinWords("You are almost to the last test"));
+        }
+  
+        [Test]
+        public static void Test6()
+        {
+            Assert.AreEqual("Just gniddik ereht is llits one more", StopGninnipsMySdrow.SpinWords("Just kidding there is still one more"));
+        }
+    }
+}
---
+++ CodeWars/Katas/26.03.2022/StopGninnipsMySdrow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeWars.Katas._26._03._2022
+{
+    public class StopGninnipsMySdrow
+    {
+        public static string SpinWords(string sentence)
+        {
+            var wordsArray = sentence
+                .Split(' ')
+                .Select(word =>
+                {
+                    if (word.Length < 5) return word;
+                    var letters = word.ToCharArray();
+                    Array.Reverse(letters);
+                    return new string(letters);
+                });
+            return string.Join(" ", wordsArray);
+        }
+    }
+}
