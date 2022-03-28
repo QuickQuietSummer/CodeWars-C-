80d83ecdd16a4a531da84b77f8492b510aa9ed4c
quickquietsummer <ruslan.kokoev.1999@gmail.com>
2022-03-28 10:38:05 +0300

    28.03.2022

---
+++ CodeWars.Test/Tests/28.03.2022/MorseCodeDecoderTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CodeWars.Katas._28._03._2022;
+using NUnit.Framework;
+
+namespace CodeWars.Test.Tests._28._03._2022
+{
+    [TestFixture]
+    public class MorseCodeDecoderTest
+    {
+        [Test]
+        public void MorseCodeDecoderBasicTest_1()
+        {
+            try
+            {
+                string input = ".... . -.--   .--- ..- -.. .";
+                string expected = "HEY JUDE";
+
+                string actual = MorseCodeDecoder.Decode(input);
+
+                Assert.AreEqual(expected, actual);
+            }
+            catch(Exception ex)
+            {
+                Assert.Fail("There seems to be an error somewhere in your code. Exception message reads as follows: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ CodeWars/Katas/28.03.2022/MorseCodeDecoder.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars.Katas._28._03._2022
+{
+    public class MorseCodeDecoder
+    {
+        public static string Decode(string morseCode)
+        {
+            var result = "";
+            var words = morseCode.Split(new[] {"  "}, StringSplitOptions.None);
+            
+            foreach (var word in words)
+            {
+                var letters = word.Trim().Split(' ');
+                var translatedWord = letters.Aggregate("", (current, letter) => current + MorseCode.Get(letter));
+
+                result += " " + translatedWord;
+            }
+
+            result = result.Trim();
+
+            return result;
+        }
+    }
+
+    public static class MorseCode
+    {
+        public static string Get(string str)
+        {
+            return _morse.FirstOrDefault(x => x.Value == str).Key.ToString();
+        }
+
+        static Dictionary<char, String> _morse = new Dictionary<char, String>()
+        {
+            {'A', ".-"},
+            {'B', "-..."},
+            {'C', "-.-."},
+            {'D', "-.."},
+            {'E', "."},
+            {'F', "..-."},
+            {'G', "--."},
+            {'H', "...."},
+            {'I', ".."},
+            {'J', ".---"},
+            {'K', "-.-"},
+            {'L', ".-.."},
+            {'M', "--"},
+            {'N', "-."},
+            {'O', "---"},
+            {'P', ".--."},
+            {'Q', "--.-"},
+            {'R', ".-."},
+            {'S', "..."},
+            {'T', "-"},
+            {'U', "..-"},
+            {'V', "...-"},
+            {'W', ".--"},
+            {'X', "-..-"},
+            {'Y', "-.--"},
+            {'Z', "--.."},
+            {'0', "-----"},
+            {'1', ".----"},
+            {'2', "..---"},
+            {'3', "...--"},
+            {'4', "....-"},
+            {'5', "....."},
+            {'6', "-...."},
+            {'7', "--..."},
+            {'8', "---.."},
+            {'9', "----."},
+        };
+    }
+}
